35480e65faec3e6777c9f517799675158afae400
raquelgothardt <71895033+raqgoth@users.noreply.github.com>
2021-01-05 11:57:18 -0500

    whisperentry.js

---
+++ client/src/components/WhisperEntry.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import WhisperForm from './WhisperForm'
+import {
+  AddWhisper,
+  RemoveWhisper,
+  CreateNewWhisper,
+  AddMeme
+} from '../store/actions/WhisperActions'
+
+const WhisperEntry = (props) => {
+  //   console.log(props)
+  const handleChange = (event) => {
+    props.createWhisper(event.target.value)
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    props.addWhisper(props.todoState.newWhisper)
+  }
+  const handleRemoveWhisper = (index) => {
+    console.log('Index of whisper to be removed', index)
+    props.removeWhisper(index)
+  }
+  const handleAddMeme = (index)=>{
+    props.addMeme(index)
+    console.log(props.todoState);
+  }
+
+  return (
+    <div>
+      <WhisperForm
+        newWhisper={props.whisperState.newWhisper.title}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+      {props.whisperState.whispers.map((whisper, index) =>(
+        <div key={index}>
+          <li>{whisper.title}</li>
+          <button onClick={() => handleRemoveWhisper(index)}>Delete</button>
+            <button onClick = {() => handleAddMeme(index)}>add meme</button> 
+        </div>
+      )
+      )}
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  //   console.log(state)
+  return {
+    whisperState: state.WhisperState
+  }
+}
+const mapActionsToProps = (dispatch) => {
+  return {
+    addWhisper: (newWhisper) => dispatch(AddWhisper(newWhisper)),
+    removeWhisper: (index) => dispatch(RemoveWhisper(index)),
+    createWhisper: (formValue) => dispatch(CreateNewWhisper(formValue)),
+    addMeme: (index) => dispatch(AddMeme(index)),
+  }
+}
+export default connect(mapStateToProps, mapActionsToProps)(WhisperEntry)
