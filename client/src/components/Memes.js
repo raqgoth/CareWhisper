657d9ab242bba9dedd598e39a433813fff171afa
raquelgothardt <71895033+raqgoth@users.noreply.github.com>
2021-01-05 12:11:11 -0500

    memes.js

---
+++ client/src/components/Memes.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const MemeForm = (props) => {
+    return (
+        <div>
+            <h3>Upload your motivational quotes memes here!</h3>
+            <p>Use imgur to upload motivational memes you find on the web. Instructions below:
+                1. go to https://imgur.com/
+                2. create an account (optional)
+                3. click on new post 
+                4. copy and paste the url from the meme, or copy and paste your img to the right
+                5. click on hidden
+                6. copy the url provided by imgur and paste it into the appropriate box below.
+            </p>
+        <form>
+            <input
+            type="varchar"
+            name="newMeme"
+            label="paste the imgur url here"
+            value={props.newMeme}
+            onChange= {props.handleChange}
+            />
+        <button type="submit" onClick={props.handleSubmit}>
+        Add Meme
+      </button>
+      </form>
+    </div>
+  )
+}
+export default MemeForm
