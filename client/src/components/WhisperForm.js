5b5c0ca75d9f7e997afd6845f96d970bb86f6ce0
raquelgothardt <71895033+raqgoth@users.noreply.github.com>
2021-01-05 11:59:17 -0500

    whisperform.js

---
+++ client/src/components/WhisperForm.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const WhisperForm = (props) => {
+    return (
+        <form>
+            <input
+            type="text"
+            name="newTitle"
+            value={props.newTitle}
+            onChange= {props.handleChange}
+            />
+            <input
+            type="text"
+            name="newWhisper"
+            value={props.newWhisper}
+            onChange={props.handleChange}
+            />
+            <input
+            type="text"
+            name="three things you are grateful for"
+            value={props.newGrateful}
+            onChange={props.handleChange}
+            />
+          <button type="submit" onClick={props.handleSubmit}>
+          Add Whisper
+        </button>
+        </form>
+    )
+}
+export default WhisperForm
